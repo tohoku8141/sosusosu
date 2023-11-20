3edd9603d7a324660306c98953d46998e6adc59f
tohoku8141 <150413148+tohoku8141@users.noreply.github.com>
2023-11-20 11:05:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function checkPrime() {
-  const userInput = document.getElementById('userInput').value;
+  const number = document.getElementById('numberInput').value;
 
-  if (userInput === '' || userInput <= 1 || !Number.isInteger(+userInput)) {
-    document.getElementById('result').innerText = 'Please enter a valid positive integer greater than 1.';
+  if (number <= 1) {
+    document.getElementById('result').innerText = 'Please enter a number greater than 1.';
     return;
   }
 
   let isPrime = true;
-  for (let i = 2; i <= Math.sqrt(userInput); i++) {
-    if (userInput % i === 0) {
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
       isPrime = false;
       break;
     }
   }
 
   if (isPrime) {
-    document.getElementById('result').innerText = `${userInput} is a prime number!`;
+    document.getElementById('result').innerText = number + ' is a prime number.';
   } else {
-    document.getElementById('result').innerText = `${userInput} is not a prime number.`;
+    document.getElementById('result').innerText = number + ' is not a prime number.';
   }
 }
